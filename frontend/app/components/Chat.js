5e7926eed45d0639275fbc831dafd788b95b76a5
Maris Karu <maris.karu@gritlab.ax>
2025-05-08 14:55:43 +0300

    adding the frontend

---
+++ frontend/app/components/Chat.js
@@ -0,0 +1,9 @@
+export default function Chat() {
+    return (
+      <div style={{ position: "fixed", bottom: 0, right: 0, width: "300px", background: "#eee" }}>
+        <h3>Chat</h3>
+        <div>Messages will appear here...</div>
+      </div>
+    );
+  }
+  
