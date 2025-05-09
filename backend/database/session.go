dbce2d6b8dcdf859c2564e23997a76cffa8d657a
Toft <toft.dah@gmail.com>
2025-05-09 23:36:51 +0300

    typos and questions

--- backend/database/session.go
+++ backend/database/session.go
@@ -10,8 +10,8 @@ func StoreSession(sessionID string, userID int, expirationTime time.Time) error 
 	// Store session ID in database
 	currentTime := time.Now().Format("2006-01-02 15:04:05")
 	_, err := db.Exec(`
-    INSERT INTO Sessions (user_id, session_token, updated_at, expires_at, creater_at) 
-    VALUES (?, ?, ?, ?, ?)`,
+    INSERT INTO Sessions (user_id, session_token, status, updated_at, expires_at, created_at) 
+    VALUES (?, ?, 'active', ?, ?, ?)`,
 		userID,
 		sessionID, // Using the same UUID for session_token
 		currentTime,
@@ -35,6 +35,9 @@ func CheckSessionExpiry(userID int) bool {
 		return false
 	}
 
+	/* Might be a misstake here..
+	_, err = db.Exec("UPDATE Sessions SET status = 'expired', updated_at = ? WHERE user_id = ?", ...)
+	according to Chat "You can’t use AND inside SET; you must use commas to separate fields being updated." -TD*/
 	if parsedTime.After(time.Now()) {
 		_, err = db.Exec("UPDATE Sessions SET status = 'expired' AND updated_at = ? WHERE user_id = ?", time.Now().Format("2006-01-02 15:04:05"), userID)
 		if err != nil {
@@ -51,6 +54,7 @@ func DeleteActiveSession(sessionID string) error {
 
 	var userID int
 
+	// Should it be Sessions or Session? -TD
 	err := db.QueryRow(`
 		UPDATE Session
 		SET status = 'deleted', updated_at = ?
@@ -67,9 +71,10 @@ func DeleteActiveSession(sessionID string) error {
 
 }
 
+// Changed WHERE to session_token from id. Feels more appropriate since we are storing UUID as session_token
 func GetSessionFromDB(sessionID string) (int, error) {
 	var userID int
-	err := db.QueryRow("SELECT user_id FROM Sessions WHERE id = ? AND status = 'active'", sessionID).Scan(&userID)
+	err := db.QueryRow("SELECT user_id FROM Sessions WHERE session_token = ? AND status = 'active'", sessionID).Scan(&userID)
 	if err != nil {
 		log.Println("No userID found for the cookie")
 		return 0, err
