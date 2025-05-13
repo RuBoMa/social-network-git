66f49ce592640e2a9cdd2cc0f5e2f22ecd37e888
Roope <roope.hongisto@gritlab.ax>
2025-05-13 17:02:23 +0300

    follower functions

--- backend/app/followers.go
+++ backend/app/followers.go
@@ -1,8 +1,20 @@
 package app
 
-// When someone has pressed "follow"
-func HandleNewFollower() {
+import (
+	"log"
+	"net/http"
+	"social_network/models"
+)
 
+// When someone has pressed "follow"
+func HandleNewFollower(w http.ResponseWriter, r *http.Request) {
+	followerID := r.URL.Query().Get("follower_id")
+	followedID := r.URL.Query().Get("followed_id")
+	if followerID == "" || followedID == "" {
+		log.Println("Error: follower_id or followed_id not provided")
+		ResponseHandler(w, http.StatusBadRequest, models.Response{Message: "Bad Request"})
+		return
+	}
 	// check if profile public or private
 
 	// IF PUBLIC
@@ -37,4 +49,32 @@ func HandleUnfollow() {
 	// Check if the user is a post_privacy table
 	// if yes, change status to deleted
 
+}
+
+func IsProfilePrivate(userID string) (bool, error) {
+	var isPrivate bool
+	query := `SELECT is_private FROM Users WHERE id = ?`
+	err := db.QueryRow(query, userID).Scan(&isPrivate)
+	return isPrivate, err
+}
+
+func AddFollowRequest(followerID, followedID string) error {
+	query := `INSERT INTO Followers (follower_id, followed_id) VALUES (?, ?)`
+	_, err := db.Exec(query, followerID, followedID)
+	return err
+}
+func AddFollower(followerID, followedID string) error {
+	query := `INSERT INTO Followers (follower_id, followed_id) VALUES (?, ?)`
+	_, err := db.Exec(query, followerID, followedID)
+	return err
+}
+func RemoveFollower(followerID, followedID string) error {
+	query := `DELETE FROM Followers WHERE follower_id = ? AND followed_id = ?`
+	_, err := db.Exec(query, followerID, followedID)
+	return err
+}
+func RemoveFollower(followerID, followedID string) error {
+	query := `DELETE FROM Followers WHERE follower_id = ? AND followed_id = ?`
+	_, err := db.Exec(query, followerID, followedID)
+	return err
 }
