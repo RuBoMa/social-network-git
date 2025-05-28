60b0a48daa036e75c91328b0b2dcaaf3de0cb441
Kira Schauman <kira.schauman@gmail.com>
2025-05-28 12:04:18 +0300

    added privacy toggle to profile

--- backend/database/users.go
+++ backend/database/users.go
@@ -260,7 +260,7 @@ func GetNonGroupMembers(groupID int) ([]models.User, error) {
 
 // UpdatePrivacySettings updates the user's privacy settings in the database
 func UpdatePrivacySettings(userID int, isPublic bool) error {
-	_, err := db.Exec("UPDATE Users SET is_public = ? AND updated_at = ? WHERE id = ?", isPublic, time.Now().Format("2006-01-02 15:04:05"), userID)
+	_, err := db.Exec("UPDATE Users SET is_public = ?, updated_at = ? WHERE id = ?", isPublic, time.Now().Format("2006-01-02 15:04:05"), userID)
 	if err != nil {
 		log.Println("Error updating privacy settings:", err)
 		return err
